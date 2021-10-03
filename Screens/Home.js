a012697128a1ab136065278a061c490645e2dcb9
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-10-03 23:29:16 +0200

    front : homepage front done

---
+++ Screens/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, SafeAreaView, StyleSheet } from "react-native";
+import Header from "../Components/Header";
+import SearchBar from "../Components/SearchBar";
+import MenuButtons from "../Components/menuButtons";
+import ContactMenu from "../Components/ContactMenu";
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={{ height: "100%" }}>
+        {/* Header */}
+        <Header />
+        {/* SearchBar */}
+        <SearchBar />
+        {/* Menu Buttons */}
+        <MenuButtons />
+        {/* Contacts Menu */}
+        <ContactMenu />
+      </SafeAreaView>
+    </View>
+  );
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1c1c1c",
+    padding: 15,
+  },
+});
